4f65780f2f216f4b30082d8384f83cc39b3deb0b
Almas <almas.roman@gmail.com>
2015-02-16 09:16:13 +0000

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,10 +26,6 @@ class User < ActiveRecord::Base
     SecureRandom.urlsafe_base64
   end
   
-   def feed
-    # Это предварительное решение. См. полную реализацию в "Following users".
-    Micropost.where("user_id = ?", id)
-  end
 
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
